73f17a21440ba4eb7a74e0518fb6046feb10fc60
Dozerth <diegocastellanos60@gmail.com>
2024-07-12 22:44:19 -0400

    ADD - Movies DAO & DTO

---
+++ Recomendador de Peliculas/DAO/MoviesDAO.cs
@@ -0,0 +1,324 @@
+﻿using MySql.Data.MySqlClient;
+using Recomendador_de_Peliculas.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Recomendador_de_Peliculas.DTO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Recomendador_de_Peliculas.DAO
+{
+    public class MoviesDAO : ConnectionDB
+    {
+        //Private fields
+        MySqlDataReader reader;
+        MySqlCommand cmd = new MySqlCommand();
+
+        #region Create
+
+        public void CreateUser(string email, string name, string passwd)
+        {
+            try
+            {
+                connection.Open();
+
+                string consult = "INSERT INTO usuarios (correo, nombre, passwd) VALUES (@Email, @Name, @Passwd)";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.Parameters.AddWithValue("@Name", name);
+                cmd.Parameters.AddWithValue("@Passwd", passwd);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        #endregion
+
+        #region Retrieve
+
+        public MovieDTO RetrieveMoviesByID(int ID)
+        {
+            MovieDTO movies = new MovieDTO();
+            try
+            {
+                connection.Open();
+
+                string consult = "SELECT * FROM peliculas WHERE peliculas_ID=@ID";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@ID", ID);
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    movies.ID = reader.GetInt32(0);
+                    movies.Title = reader.GetString(1);
+                    movies.Year = reader.GetString(2);
+                    movies.Link = reader.GetString(3);
+                    movies.Image = reader.GetString(4);
+                }
+                reader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return movies;
+        }
+
+        public List<MovieDTO> RetrieveMoviesByName(string name)
+        {
+            List<MovieDTO> movies = new List<MovieDTO>();
+            try
+            {
+                connection.Open();
+
+                string consult = "SELECT * FROM peliculas WHERE titulo LIKE @Title";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@Title", "%" + name + "%");
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        movies.Add(new MovieDTO
+                        {
+                            ID = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Year = reader.GetString(2),
+                            Link = reader.GetString(3),
+                            Image = reader.GetString(4)
+                        });
+                    }
+                }
+                reader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return movies;
+        }
+
+        public List<MovieDTO> RetrieveMoviesByGenre(int IDgenre)
+        {
+            List<MovieDTO> movies = new List<MovieDTO>();
+            try
+            {
+                connection.Open();
+
+                string consult = "SELECT p.peliculas_ID, p.titulo, p.fecha, p.link, p.imagen FROM peliculas p INNER JOIN peliculas_categorias c ON c.ID_pelicula = p.peliculas_ID  WHERE c.ID_categoria=@IDGenre GROUP BY p.peliculas_ID, p.titulo HAVING COUNT(distinct c.ID_categoria) = 1";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@IDGenre", IDgenre);
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        movies.Add(new MovieDTO
+                        {
+                            ID = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Year = reader.GetString(2),
+                            Link = reader.GetString(3),
+                            Image = reader.GetString(4)
+                        });
+                    }
+                }
+                reader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return movies;
+        }
+
+        #endregion
+
+        #region Update
+
+        public void UpdateGeneral(string username, string rol, string email)
+        {
+            try
+            {
+                connection.Open();
+
+                if (rol == "") rol = "user";
+
+                string consult = "UPDATE users SET rol=@Rol, email=@Email WHERE username=@User";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@User", username);
+                cmd.Parameters.AddWithValue("@Rol", rol);
+                cmd.Parameters.AddWithValue("@Email", email);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void UpdateUsername(string username, string newUsername)
+        {
+            try
+            {
+                connection.Open();
+
+                string consult = "UPDATE users SET username=@NewUsername WHERE username=@User";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@User", username);
+                cmd.Parameters.AddWithValue("@NewUsername", newUsername);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void UpdatePassword(string username, string passwd)
+        {
+            try
+            {
+                connection.Open();
+
+                string consult = "UPDATE users SET passwd=@Password WHERE username=@User";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@User", username);
+                cmd.Parameters.AddWithValue("@Password", passwd);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void DeleteUser(string username)
+        {
+            try
+            {
+                connection.Open();
+
+                string consult = "DETELE FROM users WHERE username=@User";
+
+                //CONFIGURING COMMAND
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@User", username);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        #endregion
+
+        #region Validations
+
+        public bool ValidateExistence(string email)
+        {
+            bool exist = false;
+            try
+            {
+                connection.Open();
+
+                string consult = "SELECT correo FROM usuarios WHERE correo=@Correo LIMIT 1";
+
+                cmd.Connection = connection;
+                cmd.Parameters.Clear();
+                cmd.CommandText = consult;
+                cmd.Parameters.AddWithValue("@Correo", email);
+                reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    exist = true;
+                }
+                reader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return exist;
+        }
+
+        #endregion
+    }
+}
---
+++ Recomendador de Peliculas/DTO/MovieDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recomendador_de_Peliculas.DTO
+{
+    public class MovieDTO
+    {
+        int _ID;
+        string _Title;
+        string _Year;
+        string _Link;
+        string _Image;
+
+        public int ID { get => _ID; set => _ID = value; }
+        public string Title { get => _Title; set => _Title = value; }
+        public string Year { get => _Year; set => _Year = value; }
+        public string Link { get => _Link; set => _Link = value; }
+        public string Image { get => _Image; set => _Image = value; }
+    }
+}
