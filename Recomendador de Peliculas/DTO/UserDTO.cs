23e360dc96155a7dc6f4b43a43eb1760cbe7eb7e
Dozerth <diegocastellanos60@gmail.com>
2024-07-12 21:36:34 -0400

    Added - USER DAO & DTO

---
+++ Recomendador de Peliculas/DTO/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recomendador_de_Peliculas.DTO
+{
+    public class UserDTO
+    {
+        int _ID;
+        string _Email;
+        string _Name;
+        string _Password;
+
+        public int ID { get => _ID; set => _ID = value; }
+        public string Email { get => _Email; set => _Email = value; }
+        public string Name { get => _Name; set => _Name = value; }
+        public string Password { get => _Password; set => _Password = value; }
+    }
+}
